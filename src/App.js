06a46adc117d7be741ed7705cddc090c81ea130d
swandzel <s.wandzel@o2.pl>
2021-05-27 18:49:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  return (
+    <div className="App">
+      <header>
+        <h1>Seb's Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
